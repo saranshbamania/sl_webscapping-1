5ac016656e5d367d099994ea27f9687decb0c51d
saransh bamania <49087533+saranshbamania-iitb@users.noreply.github.com>
2023-11-13 18:18:38 +0530

    Add files via upload

--- combined_code.py
+++ combined_code.py
@@ -1,5 +1,5 @@
 #written by saransh and prateek iit bombay
-
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,6 +9,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from PIL import Image, ImageOps, ImageEnhance
+import pytesseract
+import cv2
 
 options = Options()
 options.add_argument("--start-maximized")  # Start the window maximized
@@ -48,7 +53,7 @@ try:
     )
     
     # Type in the second input box
-    airport_to_search_second = "BOM"  # Replace with the second airport name you want to search for
+    airport_to_search_second = "BLR"  # Replace with the second airport name you want to search for
     airport_inputs[1].clear()  # Clear any pre-filled content in the second input
     airport_inputs[1].send_keys(airport_to_search_second)
     airport_inputs[1].send_keys(Keys.ENTER)
@@ -60,18 +65,33 @@ try:
     date_input.click()  # Open the date picker
 
     # Now that the date picker is open, send the date to the input
-    date_to_input = "11/20/2023"  # Replace with the date you want to enter
+    date_to_input = "10/20/2024"  # Replace with the date you want to enter
     date_input.send_keys(date_to_input)
     date_input.send_keys(Keys.ENTER)
     webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-
-    # Wait for the search button to be clickable and click it
     search_button = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Search')]"))
     )
     search_button.click()
+    time.sleep(60)
+    
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+    driver.save_screenshot("screenshot.png")
+    img = cv2.imread('screenshot.png')
+    x, y, width, height = 0, 450, 1267, 308  # Replace with your desired coordinates and dimensions
+    cropped_img = img[y:y+height, x:x+width]
+    cv2.imwrite('screenshot.png', cropped_img)
+    img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
+    img = Image.open("screenshot.png")
 
+    text = pytesseract.image_to_string(img)
+    print(text)
+    
+    
 finally:
     # Keep the browser open for inspection
     #time.sleep(10)
